5bc16ea95ec956248fb948abc188bfcb25ffbc41
Danil <danilstelle32@example.com>
2026-03-18 21:28:09 -0700

    Задание 2

--- database/migrations/2026_03_19_042746_create_users_table.php
+++ database/migrations/2026_03_19_042746_create_users_table.php
@@ -4,19 +4,24 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration {
-
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
     public function up(): void
     {
-        Schema::create('posts', function (Blueprint $table) {
+        Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name', 150)->change();
-            $table->text('text');
+            $table->timestamps();
         });
     }
 
+    /**
+     * Reverse the migrations.
+     */
     public function down(): void
     {
-        Schema::dropIfExists('posts');
+        Schema::dropIfExists('users');
     }
 };
