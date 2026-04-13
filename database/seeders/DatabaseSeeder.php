0767ed285af1a5b0bef0703cc5aa1b21503a5548
zordo-smyrf <danilstelle32@gmail.com>
2026-04-13 23:20:08 +0600

    ll

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
@@ -15,11 +16,15 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+        	DB::table('usersss')->insert([
+				'name' => 'Mark',
+				'email'  => 'marklin2gmail.com',
+				'age'  => '67',
+                'salary'  => '70000',
+                'created_at'  => '2000-04-03',
+                'updated_at'  => '2004-04-03',
+                
+			]);
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
     }
 }
