87c98ea321751d6ae2be053e39d2c0499b34625e
zordo-smyrf <danilstelle32@gmail.com>
2026-04-23 19:32:00 +0600

    lll

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,14 +16,33 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        	DB::table('usersss')->insert([
+        	DB::table('usersss')->insert(
+            [
 				'name' => 'Mark',
 				'email'  => 'marklin2gmail.com',
-				'age'  => '67',
+				'age'  => '25',
                 'salary'  => '70000',
                 'created_at'  => '2000-04-03',
-                'updated_at'  => '2004-04-03',
-			]);
+                'updated_at'  => '2004-05-04',
+			],
+            [
+				'name' => 'Jonh',
+				'email'  => 'Jonhggl3gmail.com',
+				'age'  => '18',
+                'salary'  => '50000',
+                'created_at'  => '2000-05-04',
+                'updated_at'  => '2004-06-04',
+			],
+            [
+				'name' => 'Artem',
+				'email'  => 'artemdsll4gmail.com',
+				'age'  => '29',
+                'salary'  => '90000',
+                'created_at'  => '2000-06-05',
+                'updated_at'  => '2004-07-05',
+			],
+            
+            );
 
     }
 }
