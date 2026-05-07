89b64285cfc04410ae06103bb948601a5081ca70
Danil <danilstelle32@example.com>
2026-05-07 04:07:10 -0700

    Задание 22-23

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -6,7 +6,7 @@ class UserController extends Controller
 {
     public function show1()
     {
-     $users = DB::table('userssss')->where('id', '=', 3)->get();
+     $users = DB::table('userssss')->where('id', '<', 6)->get();
 	 return view('post.show1', ['userssss' => $users]);
     }
     
--- resources/views/post/show1.blade.php
+++ resources/views/post/show1.blade.php
@@ -1,10 +1,15 @@
 <x-layout>
 	@foreach ($userssss as $user)
 		<div>
-			<h2> Имя: {{ $user->name }}</h2>
+			<h3> Имя: {{ $user->name }}</h3>
 			<div>
-			 <p> Email: {{ $user->email }} </p>
-             <p> Возраст: {{ $user->age }} </p>   
+			 <ul>
+				<li> Email: {{ $user->email }}</li>
+				<li> Возраст: {{ $user->age }}</li>
+				<li> Зарплата: {{ $user->salary }}</li>
+				<li> Дата создания: {{ $user->created_at }}</li>
+				<li> Дата обновления: {{ $user->updated_at }}</li>
+			 </ul>  
 			</div>
 		</div>
 	@endforeach
