49a19bb555c0d574d3ecd84df71ca9685b08cda3
Danil <danilstelle32@example.com>
2026-05-07 03:39:01 -0700

    Задание 17

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -6,8 +6,9 @@ class UserController extends Controller
 {
     public function show1()
     {
-     $users = DB::table('userssss')->where('salary', '=', 500)->orWhere(function($query){
-        $query->orwhere('age', '=', 20)->orwhere('age', '=', 30);})->get();
+     $users = DB::table('userssss')->where(function ($query){
+        $query->whereBetween('age', [20, 30])->orWhereBetween('salary', [400, 800]);
+    })->get();
 	 return view('post.show1', ['userssss' => $users]);
     }
     
