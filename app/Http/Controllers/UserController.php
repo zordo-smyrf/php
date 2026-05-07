6fae2f66d544d7b9fb0b0f37cc89d38f45a7321a
Danil <danilstelle32@example.com>
2026-05-07 04:25:39 -0700

    Задание 28

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -6,7 +6,7 @@ class UserController extends Controller
 {
     public function show1()
     {
-     $users = DB::table('userssss')->whereNotIn('id',[1,2,3,5] )->get();
+     $users = DB::table('userssss')->where('id','=', 3 )->get();
 	 return view('post.show1', ['userssss' => $users]);
     }
     
