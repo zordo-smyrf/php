94cad47928501ccb27bb3a727b67ca56e4d96160
Danil <danilstelle32@example.com>
2026-05-07 04:28:30 -0700

    Задание 30

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -6,7 +6,7 @@ class UserController extends Controller
 {
     public function show1()
     {
-     $users = DB::table('userssss')->where('name','=', 'Jonh' )->get();
+     $users = DB::table('userssss')->where('email','=', 'Jonhggl3gmail.com' )->get();
 	 return view('post.show1', ['userssss' => $users]);
     }
     
