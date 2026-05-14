e6c64e38c78efec39d0e916468a17bd1515a10a9
Danil <danilstelle32@example.com>
2026-05-13 22:33:55 -0700

    ll

---
+++ app/Http/Controllers/PosteController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+use Illuminate\Support\Facades\DB;
+
+class PosteController extends Controller
+{
+    public function show()
+    {
+
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,12 @@
 <?php
 
 use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\PosteController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostController;
 
 Route::get('/postssss', [PostController::class, 'show']);
 Route::get('/userssss', [UserController::class, 'show1']);
+Route::getAll('/post/all/', [PosteController::class, 'show']);
 
